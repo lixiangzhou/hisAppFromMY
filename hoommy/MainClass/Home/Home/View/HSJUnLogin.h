3fec7a90c7f7e3d6ebacf5973d75e1539476dce4
zhaicongcong <congcong.zhai@hoomsun.com>
2018-07-20 10:10:45 +0800

    调整登录注册逻辑

---
+++ hoommy/MainClass/Home/Home/View/HSJUnLogin.h
@@ -0,0 +1,13 @@
+//
+//  HSJUnLogin.h
+//  hoommy
+//
+//  Created by HXB-C on 2018/7/19.
+//  Copyright © 2018年 caihongji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HSJUnLogin : UIView
+
+@end
