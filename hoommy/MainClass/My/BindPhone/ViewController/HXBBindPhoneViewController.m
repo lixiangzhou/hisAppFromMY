9ad7bd8afe0cdbadf7fa9ea605b39eb1b67c49c3
caihongji <cai_hongji@sina.cn>
2018-07-20 19:10:27 +0800

    修复产品确认bug

--- hoommy/MainClass/My/BindPhone/ViewController/HXBBindPhoneViewController.m
+++ hoommy/MainClass/My/BindPhone/ViewController/HXBBindPhoneViewController.m
@@ -56,6 +56,9 @@
     if(self.bindPhoneStepType == HXBBindPhoneTransactionPassword) {
         self.title = @"修改交易密码";
     }
+    else if(self.bindPhoneStepType == HXBBindPhoneStepFirst) {
+        self.title = @"解绑原手机号";
+    }
     else {
         self.title = @"绑定新手机号";
     }
@@ -89,9 +92,18 @@
 - (HXBBindPhoneTableFootView *)footView {
     if(!_footView) {
         _footView = [[HXBBindPhoneTableFootView alloc] initWithFrame:CGRectMake(0, 0, kScreenWidth, kScrAdaptationH(119))];
-        _footView.buttonTitle = @"立即验证";
         NSString *phoneNo = [self.userInfoModel.userInfo.mobile replaceStringWithStartLocation:3 lenght:self.userInfoModel.userInfo.mobile.length - 7];
-        _footView.phoneInfo = phoneNo;
+        switch (self.bindPhoneStepType) {
+            case HXBBindPhoneStepFirst:
+            case HXBBindPhoneTransactionPassword:
+                _footView.buttonTitle = @"下一步";
+                _footView.phoneInfo = phoneNo;
+                break;
+            case HXBBindPhoneStepSecond:
+                _footView.buttonTitle = @"确认修改";
+                break;
+        }
+
         _footView.buttonBackGroundColor = kHXBColor_FF7055_100;
         
         kWeakSelf
