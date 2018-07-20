3144e20c4bf67e1eb0de1c05e08884f90b1adab1
zhaicongcong <congcong.zhai@hoomsun.com>
2018-07-20 19:09:40 +0800

    开发首页

--- hoommy/MainClass/Home/Home/View/HSJHomePlanView.m
+++ hoommy/MainClass/Home/Home/View/HSJHomePlanView.m
@@ -26,6 +26,8 @@
 
 @property (nonatomic, strong) UILabel *refundableLabel;
 
+@property (nonatomic, strong) UILabel *intoLabel;
+
 @end
 
 @implementation HSJHomePlanView
@@ -44,6 +46,10 @@
     [self addSubview:self.messageLabel];
     [self addSubview:self.planBackgroundImageView];
     [self addSubview:self.expectedRateLabel];
+    [self addSubview:self.annualizedTextLabel];
+    [self addSubview:self.depositoryLabel];
+    [self addSubview:self.refundableLabel];
+    [self addSubview:self.intoLabel];
     [self.titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
         make.left.equalTo(self).offset(kScrAdaptationW750(30));
         make.top.equalTo(self).offset(kScrAdaptationH750(40));
@@ -67,6 +73,28 @@
         make.centerX.equalTo(self.planBackgroundImageView);
         make.top.equalTo(self.planBackgroundImageView).offset(kScrAdaptationH750(50));
         
+    }];
+    [self.annualizedTextLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(self.expectedRateLabel);
+        make.top.equalTo(self.expectedRateLabel.mas_bottom);
+    }];
+    [self.depositoryLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.right.equalTo(self.expectedRateLabel.mas_centerX).offset(-kScrAdaptationW750(6));
+        make.top.equalTo(self.annualizedTextLabel.mas_bottom).offset(kScrAdaptationH750(30));
+        make.height.offset(kScrAdaptationH750(39));
+        make.width.offset(kScrAdaptationH750(172));
+    }];
+    [self.refundableLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.expectedRateLabel.mas_centerX).offset(kScrAdaptationW750(6));
+        make.centerY.equalTo(self.depositoryLabel);
+        make.height.offset(kScrAdaptationH750(39));
+        make.width.offset(kScrAdaptationH750(172));
+    }];
+    [self.intoLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(self.expectedRateLabel);
+        make.height.offset(kScrAdaptationH750(80));
+        make.width.offset(kScrAdaptationW750(280));
+        make.bottom.equalTo(self.planBackgroundImageView.mas_bottom).offset(-kScrAdaptationH750(50));
     }];
 }
 
@@ -123,6 +151,44 @@
         _annualizedTextLabel.textColor = kHXBColor_7F85A1_100;
     }
     return _annualizedTextLabel;
+}
+
+- (UILabel *)depositoryLabel {
+    if (!_depositoryLabel) {
+        _depositoryLabel = [[UILabel alloc] init];
+        _depositoryLabel.text = @"恒丰银行存管";
+        _depositoryLabel.textAlignment = NSTextAlignmentCenter;
+        _depositoryLabel.textColor = kHXBColor_FB7F67_100;
+        _depositoryLabel.font = kHXBFont_PINGFANGSC_REGULAR_750(24);
+        _depositoryLabel.backgroundColor = kHXBColor_FB7F67_10;
+    }
+    return _depositoryLabel;
+}
+
+- (UILabel *)refundableLabel {
+    if (!_refundableLabel) {
+        _refundableLabel = [[UILabel alloc] init];
+        _refundableLabel.text = @"7日后随时可退";
+        _refundableLabel.textAlignment = NSTextAlignmentCenter;
+        _refundableLabel.textColor = kHXBColor_FB7F67_100;
+        _refundableLabel.font = kHXBFont_PINGFANGSC_REGULAR_750(24);
+        _refundableLabel.backgroundColor = kHXBColor_FB7F67_10;
+    }
+    return _refundableLabel;
+}
+
+- (UILabel *)intoLabel {
+    if (!_intoLabel) {
+        _intoLabel = [[UILabel alloc] init];
+        _intoLabel.text = @"转入";
+        _intoLabel.textAlignment = NSTextAlignmentCenter;
+        _intoLabel.font = kHXBFont_PINGFANGSC_REGULAR_750(32);
+        _intoLabel.textColor = kHXBColor_FFFFFF_100;
+        _intoLabel.backgroundColor = kHXBColor_FF7055_100;
+        _intoLabel.layer.cornerRadius = kScrAdaptationW750(40);
+        _intoLabel.layer.masksToBounds = YES;
+    }
+    return _intoLabel;
 }
 
 @end
